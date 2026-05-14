b1fd0f74e8c277c8e2ce64f527f386037e76187f
victorpaixao06 <victorpaixao0208@gmail.com>
2026-05-13 22:45:16 -0300

    criando adaptador user

---
+++ src/main/java/com/agilis/api/infrastructure/persistence/user/UserEntity.java
@@ -0,0 +1,35 @@
+package com.agilis.api.infrastructure.persistence.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name="users")
+public class UserEntity {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
